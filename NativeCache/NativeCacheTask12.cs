e39180e3d42984cd0f562215e69068dccfeea220
sguschin <sguschin@gear-games.com>
2025-05-14 13:38:28 +0400

    AlgorithmsDataStructures Add: Добавлены ответы на задания по NativeCache

---
+++ NativeCache/NativeCacheTask12.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmsDataStructures
+{
+    class NativeCache<T>
+    {
+        public int size;
+        public string [] slots;
+        public T [] values;
+        public int [] hits;
+        
+        private int _count;
+
+        public NativeCache(int size)
+        {
+            this.size = size;
+            _count = 0;
+            slots = new string[size];
+            values = new T[size];
+            hits = new int[size];
+        }
+        
+        // Exercise 12, time complexity O(n), space complexity O(1), where n key length
+        public int HashFun(string key)
+        {
+            int hash = key.Sum(c => c);
+            
+            return Math.Abs(hash % size);
+        }
+
+        // Exercise 12, time complexity O(n), Omega(1), space complexity O(1), where n items count
+        public bool IsKey(string key)
+        {
+            int index = FindSlotIndex(key);
+            
+            return index >= 0;
+        }
+
+        // Exercise 12, time complexity O(n), Omega(1), space complexity O(1), where n items count
+        public void Put(string key, T value)
+        {
+            int hash = HashFun(key);
+            
+            for (int iteration = 0, index = hash; iteration < size; ++iteration, index = GetNextIndex(index))
+            {
+                if (slots[index] == key || slots[index] == null)
+                {
+                    AddValue(index, key, value);
+                    return;
+                }
+            }
+            
+            DisplaceItem();
+            Put(key, value);
+        }
+        
+        // Exercise 12, time complexity O(n), Omega(1), space complexity O(1), where n items count
+        public T Get(string key)
+        {
+            int index = FindSlotIndex(key);
+            
+            return index >= 0 ? values[index] : default(T);
+        }
+        
+        // Exercise 12, time complexity O(n), Omega(1), space complexity O(1), where n items count
+        public int HitsCount(string key)
+        {
+            int index = FindSlotIndex(key);
+            
+            return index >= 0 ? hits[index] : -1;
+        }
+
+        private void DisplaceItem()
+        {
+            int minValueIndex = 0;
+            int minValue = int.MaxValue;
+            
+            for (int index = 0; index < hits.Length; ++index)
+            {
+                if (hits[index] < minValue)
+                {
+                    minValueIndex = index;
+                    minValue = hits[index];
+                }
+            }
+            
+            slots[minValueIndex] = null;
+            hits[minValueIndex] = 0;
+            values[minValueIndex] = default;
+            --_count;
+        }
+
+        private void AddValue(int index, string key, T value)
+        {
+            bool isSlotEmpty = slots[index] == null;
+            
+            if (isSlotEmpty)
+            {
+                slots[index] = key;
+                values[index] = value;
+                ++_count;
+            }
+
+            if (!isSlotEmpty && !values[index].Equals(value))
+            {
+                values[index] = value;
+                hits[index] = 0;
+            }
+            
+            IncreaseValueHits(index);
+        }
+
+        private void IncreaseValueHits(int index) => ++hits[index];
+
+        private int FindSlotIndex(string key)
+        {
+            int hash = HashFun(key);
+
+            for (int iteration = 0, index = hash; iteration < size; ++iteration, index = GetNextIndex(index))
+            {
+                if (slots[index] == key)
+                {
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+        
+        private int GetNextIndex(int currentIndex)
+        {
+            return (currentIndex + 3) % size;
+        }
+    }
+}
+
---
+++ NativeCache/NativeCacheTask12Tests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AlgorithmsDataStructures
+{
+    [TestFixture]
+    public class NativeCacheTask12Tests
+    {
+        [Test]
+        public void Put_PassValueWhenDictionaryEmpty_ValueSetInExpectedIndex()
+        {
+            NativeCache<int> nativeCache = GetNativeCache();
+            string expectedKey = "Hello";
+            int expectedValue = 5;
+            
+            nativeCache.Put(expectedKey, expectedValue);
+
+            int resultValue = nativeCache.Get(expectedKey);
+            
+            Assert.That(resultValue, Is.EqualTo(expectedValue));
+        }
+        
+        [Test]
+        public void Put_PassValueWhenDictionaryExistValueWithSameKey_ValueWithKeyUpdatedToNew()
+        {
+            NativeCache<int> nativeCache = GetNativeCache();
+            string expectedKey = "Hello";
+            int oldValue = 5;
+            int expectedValue = 7;
+            
+            nativeCache.Put(expectedKey, oldValue);
+            nativeCache.Put(expectedKey, expectedValue);
+
+            int resultValue = nativeCache.Get(expectedKey);
+            
+            Assert.That(resultValue, Is.EqualTo(expectedValue));
+        }
+        
+        [Test]
+        public void Put_CacheIsFull_DisplaceValueWithMinCount()
+        {
+            NativeCache<string> nativeCache = new NativeCache<string>(17);
+
+            for (int i = 0; i < nativeCache.size; ++i)
+                nativeCache.Put($"Key_{i}", $"Value_{i}");
+            
+            for (int i = 0; i < nativeCache.size - 1; ++i)
+                nativeCache.Put($"Key_{i}", $"Value_{i}");
+
+            int displacingKeyIndex = Array.IndexOf(nativeCache.hits, 1);
+            string displacedKey = nativeCache.slots[displacingKeyIndex];
+            string newKey = $"Key_{nativeCache.size + 1}";
+            string newValue = "TestValue";
+            
+            nativeCache.Put(newKey, newValue);
+            
+            Assert.That(nativeCache.IsKey(displacedKey), Is.False);
+            Assert.That(nativeCache.IsKey(newKey), Is.True);
+            Assert.That(nativeCache.Get(newKey), Is.EqualTo(newValue));
+        }
+        
+        [Test]
+        public void Put_PassSameKeyAndValue_IncreaseHitsCount()
+        {
+            NativeCache<string> nativeCache = new NativeCache<string>(17);
+            
+            for (int i = 0; i < nativeCache.size; ++i)
+                nativeCache.Put($"Key_{i}", $"Value_{i}");
+            
+            string keyForIncrease = $"Key_0";
+            string valueForIncrease = $"Value_0";
+            int expectedHitsCount = 2;
+            
+            nativeCache.Put(keyForIncrease, valueForIncrease);
+            
+            Assert.That(nativeCache.Get(keyForIncrease), Is.EqualTo(valueForIncrease));
+            Assert.That(nativeCache.HitsCount(keyForIncrease), Is.EqualTo(expectedHitsCount));
+        }
+
+        [Test]
+        public void IsKey_WhenKeyExists_ReturnTrue()
+        {
+            NativeCache<int> nativeCache = GetNativeCache();
+            string key = "Hello";
+            int value = 7;
+            
+            nativeCache.Put(key, value);
+
+            bool isKeyResult = nativeCache.IsKey(key);
+            
+            Assert.That(isKeyResult, Is.True);
+        }
+        
+        [Test]
+        public void IsKey_WhenKeyDoesNotExists_ReturnFalse()
+        {
+            NativeCache<int> nativeCache = GetNativeCache();
+            string key = "Hello";
+            string findingKey = "World";
+            int value = 7;
+            
+            nativeCache.Put(key, value);
+
+            bool isKeyResult = nativeCache.IsKey(findingKey);
+            
+            Assert.That(isKeyResult, Is.False);
+        }
+        
+        [Test]
+        public void Get_WhenKeyValueExists_ReturnValue()
+        {
+            NativeCache<int> nativeCache = GetNativeCache();
+            string key = "Hello";
+            int expectedValue = 7;
+            
+            nativeCache.Put(key, expectedValue);
+
+            int valueResult = nativeCache.Get(key);
+            
+            Assert.That(valueResult, Is.EqualTo(expectedValue));
+        }
+        
+        [Test]
+        public void Get_WhenKeyValueDoesNotExists_ReturnNull()
+        {
+            NativeCache<object> nativeCache = new NativeCache<object>(17);
+            string key = "Hello";
+            string findingKey = "World";
+            object value = new object();
+            
+            nativeCache.Put(key, value);
+
+            object isKeyResult = nativeCache.Get(findingKey);
+            
+            Assert.That(isKeyResult, Is.Null);
+        }
+        
+        private static NativeCache<int>GetNativeCache()
+        {
+            return new NativeCache<int>(17);
+        }
+    }
+}
